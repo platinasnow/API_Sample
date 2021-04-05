45506b9b1381b1e900e0efd7f2f65866cea0c7d8
platinasnow <platinasnow@naver.com>
2021-04-05 11:43:26 +0900

    JWT Token 테스트 수정

--- src/test/java/org/api/sample/JwtTokenTests.java
+++ src/test/java/org/api/sample/JwtTokenTests.java
@@ -2,12 +2,10 @@ package org.api.sample;
 
 import org.api.sample.security.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest(properties = {"classpath:test-application.yml"})
@@ -16,20 +14,28 @@ public class JwtTokenTests {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
     private final Logger logger = LoggerFactory.getLogger(JwtTokenTests.class);
-    private MockMvc mockMvc;
+    private static String token;
 
     @Test
     public void createTokenTest(){
         String token = jwtTokenProvider.createJwtToken("test", null);
         logger.info("token ===>{}", token);
+        this.token = token;
         assert (token != null);
     }
 
     @Test
-    public void getTokenBodyTest(){
+    public void validateExpiredTokenTest(){
         String expiredToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MTIzIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImV4cCI6MTYxNzE3OTM2NCwiaWF0IjoxNjE3MTc3NTY0fQ.BZIvRyFr1pNiEw62nq1y8eHxejpJQNPfAbIQlbG5qFI";
         String primaryKey = jwtTokenProvider.getUserPrimaryKey(expiredToken);
         assert (primaryKey == null);
+    }
+
+    @Test
+    public void getTokenBodyTest(){
+        String primaryKey = jwtTokenProvider.getUserPrimaryKey(this.token);
+        logger.info("primaryKey ===>{}", primaryKey);
+        assert ("test".equals(primaryKey));
     }
 
 
